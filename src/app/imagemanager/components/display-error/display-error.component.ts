7c48d8edeeb0e4177436d3a39364953b7ca0f4ed
Dario Mitev <82581946+dmitev2000@users.noreply.github.com>
2023-12-18 16:48:59 +0100

    Handle errors in image-details

--- src/app/imagemanager/components/display-error/display-error.component.ts
+++ src/app/imagemanager/components/display-error/display-error.component.ts
@@ -3,17 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-display-error',
   templateUrl: './display-error.component.html',
-  styleUrls: ['./display-error.component.scss']
+  styleUrls: ['./display-error.component.scss'],
 })
 export class DisplayErrorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   @Input() errorMessage: string;
   @Input() errorStatus: number;
-  @Input() errorType: string;
-  
 }
